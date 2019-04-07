f3f990215173137d37320e4f4c07ad4d3e21a731
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-07 13:46:20 +1000

    Initial commit

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace OsuFancyDance
+namespace Web
 {
     public class Startup
     {
