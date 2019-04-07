603eefef694c78849d819d8c33c19c4892381555
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-07 14:33:55 +1000

    Client connecting to websocket

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,34 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.WebSockets;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using WebSocketManager;
 
 namespace Web
 {
     public class Startup
     {
+        private WebSocket _webSocket;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddWebSocketManager();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // TODO: start the osu presenter
+            var osuPresenter = new OsuStatePresenter.OsuPresenter();
 
-            app.Run(async (context) =>
+            // serve static files from exe output directory
+            var currentExeDirectory = Path.Combine(Environment.CurrentDirectory);
+            app.UseFileServer(new FileServerOptions
             {
-                await context.Response.WriteAsync("Hello World!");
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(currentExeDirectory, "public")),
+                RequestPath = "/Osu",
+                EnableDirectoryBrowsing = false
             });
+
+            // init web sockets
+            app.UseWebSockets();
+            app.MapWebSocketManager("/ws", serviceProvider.GetService<Osu.OsuMessageHandler>());
+
+            // init request handling
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        //WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        _webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+                        //await WebSocketEcho(context, _webSocket);
+                        byte[] buffer = Encoding.ASCII.GetBytes("test string");
+                        //await _webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                        await _webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, 1024), WebSocketMessageType.Text, endOfMessage: true, CancellationToken.None);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+
+                await next.Invoke();
+
+                // redirect root
+                if (context.Request.GetEncodedPathAndQuery() == "/")
+                {
+                    context.Response.Redirect("/Osu");
+                }
+            });
+
+            // TODO: Add 404 page to every request except for "/Osu"
+        }
+
+        private async Task WebSocketEcho(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
---
+++ Web/public/js/app.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    // x-gif: http://geelen.github.io/x-gif
+
+    var content = document.getElementById("content");
+
+    var socket = new WebSocket(GetWebSocketUrl("ws"));
+
+    socket.onopen = (event) => {
+        console.log("Socket opened, hopefully.")
+    }
+
+    socket.onmessage = (event) => {
+        console.log("Message received:")
+        console.log("origin", event.origin);
+        console.log("data", event.data);
+    }
+
+    // TODO: Handle automatic reconnect trying
+
+})();
+
+function GetWebSocketUrl(pathName) {
+    var loc = window.location, new_uri;
+    if (loc.protocol === "https:") {
+        new_uri = "wss:";
+    } else {
+        new_uri = "ws:";
+    }
+    new_uri += "//" + loc.host + "/" + pathName;
+
+    return new_uri;
+}
