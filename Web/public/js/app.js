51eb63491793d796bdd1a6a8f3daccf0efc9b2bb
Ceiling Waffle <ceilingwaffle@gmail.com>
2019-04-10 20:05:31 +1000

    Handle websocket automatic reconnect

--- Web/public/js/app.js
+++ Web/public/js/app.js
@@ -1,14 +1,28 @@
 ﻿(function () {
+    connect();
+})();
+
+function connect() {
     // x-gif: http://geelen.github.io/x-gif
 
-    var content = document.getElementById("content");
+    //var content = document.getElementById("content");
     var socket = new WebSocket(GetWebSocketUrl("Osu"));
 
     socket.onopen = (event) => {
         console.log("Socket opened, hopefully.")
-
-        // TODO: Fetch current BPM
     }
+
+    socket.onclose = function (e) {
+        console.log('Socket is closed. Reconnect will be attempted in 1 second.', e.reason);
+        setTimeout(function () {
+            connect();
+        }, 1000);
+    };
+
+    socket.onerror = function (err) {
+        console.error('Socket encountered error: ', err.message, 'Closing socket');
+        socket.close();
+    };
 
     socket.onmessage = (event) => {
         console.log("Message received from origin:" + event.origin);
@@ -24,9 +38,9 @@
         //console.log("gifElement", gifElement);
     };
 
-    // TODO: Handle automatic reconnect trying
     // TODO: Handle 0 bpm (just play gif at its normal speed?)
-})();
+}
+
 
 function ChangeGifSpeed(gifElement, bpm, isPaused) {
 
@@ -35,10 +49,14 @@ function ChangeGifSpeed(gifElement, bpm, isPaused) {
     // TODO: stabilize sudden bpm changes
     //bpm /= 8;
 
-    if (isPaused == true)
+    if (isPaused == true) {
+        console.log("Paused gif.");
         gifElement.setAttribute("stopped", "");
-    else
+    }
+    else {
+        console.log("Resumed gif.");
         gifElement.removeAttribute("stopped");
+    }
 
     gifElement.setAttribute("bpm", bpm.toString());
     console.log("Changed gif speed to " + bpm.toString());
